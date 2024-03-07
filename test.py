9b020942ba525c99391b03143d21e25138f5a4ff
sunjiaqi52777 <152960480+sunjiaqi52777@users.noreply.github.com>
2024-03-07 16:37:32 +0800

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,39 @@ import statistics
 
 from multimetric_new.cls.base_stats import MetricBaseStats
 
+class MetricBaseStatsAverage(MetricBaseStats):
 
+    def __init__(self):
+        # comment
+        super().__init__()
+
+    def _getInputList(self, metrics, key):
+        res = []
+        for _, v in metrics.items():
+            if key in v and not isinstance(v[key], list):
+                res.append(v[key])
+        return res
+
+    def get_results(self, metrics, files="files", overall="overall"):
+        _keylist = list(metrics[overall].keys())
+        metrics["stats"] = {}
+        metrics["stats"]["mean"] = {}
+        metrics["stats"]["max"] = {}
+        metrics["stats"]["min"] = {}
+        if len(metrics["files"]) > 1:
+            metrics["stats"]["sd"] = {}
+        metrics["stats"]["median"] = {}
+        for k in _keylist:
+            _list = self._getInputList(metrics[files], k)
+            if not _list:
+                continue
+            metrics["stats"]["mean"][k] = statistics.mean(_list)
+            metrics["stats"]["median"][k] = statistics.median(_list)
+            metrics["stats"]["max"][k] = max(_list)
+            metrics["stats"]["min"][k] = min(_list)
+            if len(metrics["files"]) > 1:
+                metrics["stats"]["sd"][k] = statistics.stdev(_list)
+        return super().get_results(metrics, files="files", overall="overall")
 class MetricBaseStatsAverage(MetricBaseStats):
 
     def __init__(self):
